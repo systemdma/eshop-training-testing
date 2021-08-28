f832f4bb2c097ff0e61171b8cb8a6a156f5da915
systemdma <dystopianworld@protonmail.com>
2021-08-28 19:19:48 +0300

    Add requirements file, conftest file and test_main_page.py file (only for test, rewrite later)

---
+++ conftest.py
@@ -0,0 +1,32 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        default="en",
+        help="choose language")
+    parser.addoption(
+        "--browser",
+        default="chrome",
+        help="choose browser: chrome or firefox"
+    )
+
+
+@pytest.fixture
+def browser(request):
+    if request.config.getoption("--browser") == "chrome":
+        browser = webdriver.Chrome()
+    elif request.config.getoption("--browser") == "firefox":
+        browser = webdriver.Firefox()
+    else:
+        raise ValueError("Uncorrect browser name")
+    yield browser
+    browser.quit()
+
+
+@pytest.fixture
+def get_choose_language(request):
+    return request.config.getoption("--language")
+    
